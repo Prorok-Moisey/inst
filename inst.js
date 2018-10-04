0daf59c263178d908867126fb2fab349fdd1d189
Vladimir <vyakin97@mail.ru>
2018-10-04 19:22:06 +0300

    ну.. сделал кривую строку поиска, нерабочий крестик и хэдер, блочащий всю страницу

--- inst.js
+++ inst.js
@@ -1,1 +1,20 @@
- 
+$(document).ready(function(){
+ 	$("input").addClass("onblur_input");
+ 	$(".input_image").hide();
+
+ 	$("input").blur(function(){
+ 		$("input").removeClass("onfocus_input").addClass("onblur_input");
+ 		$(".input_image").hide();
+ 	});
+
+ 	$("input").focus(function(){
+ 		$("input").removeClass("onblur_input").addClass("onfocus_input");
+ 		$(".input_image").show();
+ 	});
+
+ 	$(".input_image").click(function(){
+ 		alert('asdfg');
+ 		$("input").reset();
+ 	});
+ 	
+});

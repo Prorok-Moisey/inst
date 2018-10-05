b0c4b3ac2027596996718318084fa4841ce1b57d
Vladimir <vyakin97@mail.ru>
2018-10-05 17:21:33 +0300

    сделан более-менее скрол, инпут не решён, изменение ширины тоже

--- inst.js
+++ inst.js
@@ -3,18 +3,32 @@ $(document).ready(function(){
  	$(".input_image").hide();
 
  	$("input").blur(function(){
- 		$("input").removeClass("onfocus_input").addClass("onblur_input");
+ 		$("input").removeClass("onfocus_input").addClass("onblur_input");//инпут без фокуса
  		$(".input_image").hide();
  	});
 
  	$("input").focus(function(){
- 		$("input").removeClass("onblur_input").addClass("onfocus_input");
+ 		$("input").removeClass("onblur_input").addClass("onfocus_input");//инпут под фокусом
  		$(".input_image").show();
  	});
 
- 	$(".input_image").click(function(){
+ 	$(".input_image").click(function(){//нажатие на крестик
  		alert('asdfg');
  		$("input").reset();
  	});
  	
+ 	$(window).scroll(function(){
+ 		if($(this).scrollTop()>1){
+ 			$(".header").addClass("header_scroll");
+ 			$(".header_fixed").addClass("header_scroll");
+ 			//$(".logo2").hide();
+ 			$(".logo2").addClass("logo2_scroll");
+ 		}
+ 		else{
+ 			$(".header").removeClass("header_scroll");
+ 			$(".header_fixed").removeClass("header_scroll");	
+ 			//$(".logo2").show();
+ 			$(".logo2").removeClass("logo2_scroll");
+ 		}
+ 	});
 });

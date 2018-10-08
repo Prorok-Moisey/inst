abd67ca926bbeaecef6b6911536ddc986555465c
Vladimir <vyakin97@mail.ru>
2018-10-08 19:09:13 +0300

    начата работа над верхней частью, но что-то всё как-то очень сложно

--- inst.js
+++ inst.js
@@ -1,7 +1,14 @@
-$(document).ready(function(){
+$(document).ready(function(){				
+									/*начальные установки*/
  	$("input").addClass("onblur_input");
  	$(".input_image").hide();
-
+ 	if ($(window).width() < 500){
+ 			$("input").hide();
+ 	}
+ 	if ($(window).width() < 650){
+ 		$(".logo2").hide();
+ 	}
+ 									/*конец начальных установок*/
  	$("input").blur(function(){
  		$("input").removeClass("onfocus_input").addClass("onblur_input");//инпут без фокуса
  		$(".input_image").hide();
@@ -16,19 +23,39 @@ $(document).ready(function(){
  		alert('asdfg');
  		$("input").reset();
  	});
- 	
- 	$(window).scroll(function(){
+
+ 	$(window).scroll(function(){				//уменьшение фиксированного хэдера при скроле
  		if($(this).scrollTop()>1){
- 			$(".header").addClass("header_scroll");
- 			$(".header_fixed").addClass("header_scroll");
- 			//$(".logo2").hide();
- 			$(".logo2").addClass("logo2_scroll");
+ 			$(".header, .header_fixed").addClass("header_scroll");
+ 			$(".logo2").animate({'opacity':'0'},0.4);
  		}
  		else{
- 			$(".header").removeClass("header_scroll");
- 			$(".header_fixed").removeClass("header_scroll");	
- 			//$(".logo2").show();
- 			$(".logo2").removeClass("logo2_scroll");
+ 			$(".header, .header_fixed").removeClass("header_scroll");
+ 			$(".logo2").animate({'opacity':'1'},0.4)
  		}
  	});
+
+ 	$(window).resize(function(){				//изменение ширины
+ 		if ($(window).width() < 840){
+ 			$(".footer__string, .links").addClass("text_center");
+ 		}
+ 		else{
+ 			$(".footer__string, .links").removeClass("text_center");
+ 		}
+
+ 		if ($(window).width() < 650){
+ 			$(".logo2").hide(400);
+ 		}
+ 		else{
+ 			$(".logo2").show(400);	
+ 		}
+ 		
+ 		if ($(window).width() < 500){
+ 			$("input").hide(400);
+ 		}
+ 		else{
+ 			$("input").show(400);	
+ 		}
+ 	});
+
 });
